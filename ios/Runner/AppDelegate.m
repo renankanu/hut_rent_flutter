fc6d434eb57f3ea95ba28bcd2a349b81bc06d2ee
renankanu <renankanu@gmail.com>
2019-07-31 12:40:48 -0300

    Remove key from Google Maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [GMSServices provideAPIKey:@"AIzaSyCQrFtJrwsa6_mdAtV_ZyFlKQQCeBaMLic"];
+    [GMSServices provideAPIKey:@""];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
